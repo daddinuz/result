ca1dda828802b64efdb870f4526bf67d89e375c2
daddinuz <daddinuz@gmail.com>
2019-12-27 18:41:00 +0100

    tests

--- tests/unit/features.c
+++ tests/unit/features.c
@@ -1,300 +1,99 @@
 /*
-Author: daddinuz
-email:  daddinuz@gmail.com
-
-Copyright (c) 2018 Davide Di Carlo
-
-Permission is hereby granted, free of charge, to any person
-obtaining a copy of this software and associated documentation
-files (the "Software"), to deal in the Software without
-restriction, including without limitation the rights to use,
-copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be
-included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-OTHER DEALINGS IN THE SOFTWARE.
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2019 Davide Di Carlo
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
  */
 
-#include <result.h>
 #include <traits/traits.h>
 #include "features.h"
 
-Feature(Result_error) {
-    const size_t counter = traitsUnit_getWrappedSignalsCounter();
-
-    traits_unit_wraps(SIGABRT) {
-        Error error = NULL;
-        Result _ = Result_error(error);
-        (void) _;
-    }
-    assert_equal(traitsUnit_getWrappedSignalsCounter(), counter + 1);
-
-    traits_unit_wraps(SIGABRT) {
-        Result _ = Result_error(Ok);
-        (void) _;
-    }
-    assert_equal(traits_unit_get_wrapped_signals_counter(), counter + 2);
-
-    Result sut = Result_error(DomainError);
-    assert_true(Result_isError(sut));
-    assert_false(Result_isOk(sut));
-    assert_equal(DomainError, Result_inspect(sut));
-    assert_string_equal(Error_explain(DomainError), Result_explain(sut));
-}
+ResultDefine(DivisionResult, const char *, double)
 
 Feature(Result_ok) {
-    const size_t counter = traits_unit_get_wrapped_signals_counter();
+    const double value = 5.0;
+    struct DivisionResult sut = DivisionResult_ok(value);
 
-    traits_unit_wraps(SIGABRT) {
-        Result _ = Result_ok(NULL);
-        (void) _;
-    }
-    assert_equal(traits_unit_get_wrapped_signals_counter(), counter + 1);
+    assert_that(DivisionResult_isOk(sut));
+    assert_false(DivisionResult_isErr(sut));
 
-    const char value[] = "A";
-    Result sut = Result_ok(value);
-    assert_false(Result_isError(sut));
-    assert_true(Result_isOk(sut));
-    assert_equal(Ok, Result_inspect(sut));
-    assert_string_equal(Error_explain(Ok), Result_explain(sut));
-    assert_equal(Result_unwrap(sut), value);
-}
-
-Feature(Result_fromNullable) {
     {
-        Result sut = Result_fromNullable(NULL);
-        assert_true(Result_isError(sut));
-        assert_false(Result_isOk(sut));
-        assert_equal(NullReferenceError, Result_inspect(sut));
-        assert_string_equal(Error_explain(NullReferenceError), Result_explain(sut));
+        const double ok = DivisionResult_unwrap(sut);
+        assert_equal(value, ok);
     }
 
     {
-        const char value[] = "A";
-        Result sut = Result_fromNullable(value);
-        assert_false(Result_isError(sut));
-        assert_true(Result_isOk(sut));
-        assert_equal(Ok, Result_inspect(sut));
-        assert_string_equal(Error_explain(Ok), Result_explain(sut));
-        assert_equal(Result_unwrap(sut), value);
+        const double ok = DivisionResult_expect(sut, "expected a value");
+        assert_equal(value, ok);
     }
+
+    const size_t sigcount = traitsUnit_getWrappedSignalsCounter();
+
+    traitsUnit_wrap(SIGABRT) {
+        const char *const err = DivisionResult_unwrapErr(sut);
+        (void) err;
+    }
+
+    assert_equal(sigcount + 1, traitsUnit_getWrappedSignalsCounter());
+
+    traitsUnit_wrap(SIGABRT) {
+        const char *const err = DivisionResult_expectErr(sut, "expected an error");
+        (void) err;
+    }
+
+    assert_equal(sigcount + 2, traitsUnit_getWrappedSignalsCounter());
 }
 
-const void *mapFromError(const void *_) {
-    (void) _;
-    assert_true(false);
-    return NULL;
-}
+Feature(Result_err) {
+    const char *const value = "error";
+    struct DivisionResult sut = DivisionResult_err(value);
 
-const void *mapFromOkToNull(const void *_) {
-    (void) _;
-    return NULL;
-}
+    assert_that(DivisionResult_isErr(sut));
+    assert_false(DivisionResult_isOk(sut));
 
-const void *mapOk(const void *_) {
-    (void) _;
-    return "B";
-}
-
-Feature(Result_map) {
     {
-        const Result sut = Result_map(Result_error(DomainError), mapFromError);
-        assert_true(Result_isError(sut));
-        assert_false(Result_isOk(sut));
-        assert_equal(DomainError, Result_inspect(sut));
-        assert_string_equal(Error_explain(DomainError), Result_explain(sut));
+        const char *const err = DivisionResult_unwrapErr(sut);
+        assert_equal(value, err);
     }
 
     {
-        const Result sut = Result_map(Result_ok("A"), mapFromOkToNull);
-        assert_true(Result_isError(sut));
-        assert_false(Result_isOk(sut));
-        assert_equal(NullReferenceError, Result_inspect(sut));
-        assert_string_equal(Error_explain(NullReferenceError), Result_explain(sut));
+        const char *const err = DivisionResult_expectErr(sut, "expected a value");
+        assert_equal(value, err);
     }
 
-    {
-        const Result sut = Result_map(Result_ok("A"), mapOk);
-        assert_false(Result_isError(sut));
-        assert_true(Result_isOk(sut));
-        assert_equal(Ok, Result_inspect(sut));
-        assert_string_equal(Error_explain(Ok), Result_explain(sut));
-        assert_string_equal(Result_unwrap(sut), "B");
-    }
-}
+    const size_t sigcount = traitsUnit_getWrappedSignalsCounter();
 
-Result chainFromError(const void *_) {
-    (void) _;
-    assert_true(false);
-    return Result_error(IllegalState);
-}
-
-Result chainFromOkToError(const void *_) {
-    (void) _;
-    return Result_error(DomainError);
-}
-
-Result chainOk(const void *_) {
-    (void) _;
-    return Result_ok("B");
-}
-
-Feature(Result_chain) {
-    {
-        const Result sut = Result_chain(Result_error(DomainError), chainFromError);
-        assert_true(Result_isError(sut));
-        assert_false(Result_isOk(sut));
-        assert_equal(DomainError, Result_inspect(sut));
-        assert_string_equal(Error_explain(DomainError), Result_explain(sut));
+    traitsUnit_wrap(SIGABRT) {
+        const double value = DivisionResult_unwrap(sut);
+        (void) value;
     }
 
-    {
-        const Result sut = Result_chain(Result_ok("A"), chainFromOkToError);
-        assert_true(Result_isError(sut));
-        assert_false(Result_isOk(sut));
-        assert_equal(DomainError, Result_inspect(sut));
-        assert_string_equal(Error_explain(DomainError), Result_explain(sut));
+    assert_equal(sigcount + 1, traitsUnit_getWrappedSignalsCounter());
+
+    traitsUnit_wrap(SIGABRT) {
+        const double value = DivisionResult_expect(sut, "expected a value");
+        (void) value;
     }
 
-    {
-        const Result sut = Result_chain(Result_ok("A"), chainOk);
-        assert_false(Result_isError(sut));
-        assert_true(Result_isOk(sut));
-        assert_equal(Ok, Result_inspect(sut));
-        assert_string_equal(Error_explain(Ok), Result_explain(sut));
-        assert_string_equal(Result_unwrap(sut), "B");
-    }
-}
-
-Feature(Result_alt) {
-    const Result errorAlternative = Result_error(MathError);
-    const Result okAlternative = Result_ok("X");
-
-    {
-        const Result sut = Result_alt(Result_error(DomainError), errorAlternative);
-        assert_true(Result_isError(sut));
-        assert_false(Result_isOk(sut));
-        assert_equal(MathError, Result_inspect(sut));
-        assert_string_equal(Error_explain(MathError), Result_explain(sut));
-    }
-
-    {
-        const Result sut = Result_alt(Result_error(DomainError), okAlternative);
-        assert_false(Result_isError(sut));
-        assert_true(Result_isOk(sut));
-        assert_equal(Ok, Result_inspect(sut));
-        assert_string_equal(Error_explain(Ok), Result_explain(sut));
-        assert_string_equal(Result_unwrap(sut), "X");
-    }
-
-    {
-        const Result sut = Result_alt(Result_ok("A"), okAlternative);
-        assert_false(Result_isError(sut));
-        assert_true(Result_isOk(sut));
-        assert_equal(Ok, Result_inspect(sut));
-        assert_string_equal(Error_explain(Ok), Result_explain(sut));
-        assert_string_equal(Result_unwrap(sut), "A");
-    }
-}
-
-Result orElseError(void) {
-    return Result_error(MathError);
-}
-
-Result orElseOk(void) {
-    return Result_ok("X");
-}
-
-Feature(Result_orElse) {
-    {
-        const Result sut = Result_orElse(Result_error(DomainError), orElseError);
-        assert_true(Result_isError(sut));
-        assert_false(Result_isOk(sut));
-        assert_equal(MathError, Result_inspect(sut));
-        assert_string_equal(Error_explain(MathError), Result_explain(sut));
-    }
-
-    {
-        const Result sut = Result_orElse(Result_error(DomainError), orElseOk);
-        assert_false(Result_isError(sut));
-        assert_true(Result_isOk(sut));
-        assert_equal(Ok, Result_inspect(sut));
-        assert_string_equal(Error_explain(Ok), Result_explain(sut));
-        assert_string_equal(Result_unwrap(sut), "X");
-    }
-
-    {
-        const Result sut = Result_orElse(Result_ok("A"), orElseOk);
-        assert_false(Result_isError(sut));
-        assert_true(Result_isOk(sut));
-        assert_equal(Ok, Result_inspect(sut));
-        assert_string_equal(Error_explain(Ok), Result_explain(sut));
-        assert_string_equal(Result_unwrap(sut), "A");
-    }
-}
-
-Feature(Result_unwrap) {
-    Result sut = Result_ok("A");
-
-    const char *value = Result_unwrap(sut);
-    assert_string_equal(value, "A");
-
-    const size_t counter = traits_unit_get_wrapped_signals_counter();
-    traits_unit_wraps(SIGABRT) {
-        const char *_ = Result_unwrap(Result_error(DomainError));
-        (void) _;
-    }
-    assert_equal(traits_unit_get_wrapped_signals_counter(), counter + 1);
-}
-
-Feature(Result_unwrapAsMutable) {
-    char value[] = "A";
-    Result sut = Result_ok(value);
-
-    assert_equal(value, Result_unwrapAsMutable(sut));
-
-    const size_t counter = traits_unit_get_wrapped_signals_counter();
-    traits_unit_wraps(SIGABRT) {
-        const char *_ = Result_unwrap(Result_error(DomainError));
-        (void) _;
-    }
-    assert_equal(traits_unit_get_wrapped_signals_counter(), counter + 1);
-}
-
-Feature(Result_expect) {
-    Result sut = Result_ok("A");
-
-    const char *value = Result_expect(sut, "%s", "Expected a value");
-    assert_string_equal(value, "A");
-
-    const size_t counter = traits_unit_get_wrapped_signals_counter();
-    traits_unit_wraps(SIGABRT) {
-        const char *_ = Result_expect(Result_error(DomainError), "%s", "Expected a value");
-        (void) _;
-    }
-    assert_equal(traits_unit_get_wrapped_signals_counter(), counter + 1);
-}
-
-Feature(Result_expectAsMutable) {
-    char value[] = "A";
-    Result sut = Result_ok(value);
-
-    assert_equal(value, Result_expectAsMutable(sut, "%s", "Expected a value"));
-
-    const size_t counter = traits_unit_get_wrapped_signals_counter();
-    traits_unit_wraps(SIGABRT) {
-        const char *_ = Result_expect(Result_error(DomainError), "%s", "Expected a value");
-        (void) _;
-    }
-    assert_equal(traits_unit_get_wrapped_signals_counter(), counter + 1);
+    assert_equal(sigcount + 2, traitsUnit_getWrappedSignalsCounter());
 }
--- tests/unit/features.h
+++ tests/unit/features.h
@@ -1,53 +1,43 @@
 /*
-Author: daddinuz
-email:  daddinuz@gmail.com
-
-Copyright (c) 2018 Davide Di Carlo
-
-Permission is hereby granted, free of charge, to any person
-obtaining a copy of this software and associated documentation
-files (the "Software"), to deal in the Software without
-restriction, including without limitation the rights to use,
-copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be
-included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-OTHER DEALINGS IN THE SOFTWARE.
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2019 Davide Di Carlo
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
  */
 
 #pragma once
 
+#include <result.h>
 #include <traits-unit/traits-unit.h>
-
-#if !(defined(__GNUC__) || defined(__clang__))
-__attribute__(...)
-#endif
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-Feature(Result_error);
+ResultDeclare(DivisionResult, const char *, double)
+
 Feature(Result_ok);
-Feature(Result_fromNullable);
-Feature(Result_map);
-Feature(Result_chain);
-Feature(Result_alt);
-Feature(Result_orElse);
-Feature(Result_unwrap);
-Feature(Result_unwrapAsMutable);
-Feature(Result_expect);
-Feature(Result_expectAsMutable);
+Feature(Result_err);
 
 #ifdef __cplusplus
 }
